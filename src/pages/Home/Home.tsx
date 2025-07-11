14d2a02da5448d56b1a05389120512f52b8afde9
VivySilva <77069795+VivySilva@users.noreply.github.com>
2025-07-11 00:22:57 -0300

    Page Home - 90%

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,11 @@
 // import './Home.module.css';
 import styles from './Home.module.css';
 import logoImg from '../../assets/Logo.svg';
+import { BsCardText } from "react-icons/bs";
+import { BsBoundingBoxCircles } from 'react-icons/bs';
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { IoLogoGithub } from "react-icons/io5";
+
 
 export default function Home() {
     return (
@@ -12,11 +17,50 @@ export default function Home() {
                         <h1>SBoard</h1>
                     </div>
                     <h2>Dashboard for managing sliced networks</h2>
+                    <div className={styles.line} />
                 </div>
-            
-            
-            
-            </div>
+
+                <div className={styles.conteniner_left}>
+                    <div className={styles.information}>
+                        <div className={styles.topic}>
+                            <BsCardText size={30}/>
+                            <h4>Description</h4>
+                        </div>
+                        <p>SBoard is an interactive web platform designed to visualize, 
+                            simulate and manage the service cycle of virtual networks 
+                            within the Network Slicing concept in 5G. The application 
+                            allows users to analyze the operation of different network 
+                            slices, track performance metrics and test resource allocation 
+                            scenarios dynamically.</p>
+                    </div>
+                
+                    <div className={styles.information}>
+                        <div className={styles.topic}>
+                            <BsBoundingBoxCircles size={30}/>
+                            <h4>My projects</h4>
+                        </div>
+                        <p>Access your latest projects created</p>
+                    </div>
+
+                </div>
+                    <div className={styles.container_right}>
+                        <div className={styles.information}>
+                            <div className={styles.topic}>
+                                <IoDocumentTextOutline size={30}/>
+                                <h4>Documentation</h4>
+                            </div>
+                            <a href="https://github.com/wellington-tinho/SBoard/wiki">https://github.com/wellington-tinho/SBoard/wiki</a>
+                        </div>
+
+                        <div className={styles.information}>
+                            <div className={styles.topic}>
+                                <IoLogoGithub size={30}/>
+                                <h4>GitHub</h4>
+                            </div>
+                            <a href="https://github.com/wellington-tinho/SBoard">https://github.com/wellington-tinho/SBoard</a>
+                        </div> 
+                    </div>
+                </div>
         </>
     );
 }
