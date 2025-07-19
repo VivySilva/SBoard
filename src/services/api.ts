216daa7c8af3ac7799f8ae87679849a21e815823
VivySilva <77069795+VivySilva@users.noreply.github.com>
2025-07-19 12:34:21 -0300

    Preparação para o back e bd

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
- 
+
 var API
 if (process.env.NODE_ENV !== 'production') {
-   API = ('http://127.0.0.1:5000/')
-  
+  // Em desenvolvimento, usamos o caminho relativo do proxy.
+  // O Vite irá redirecionar isso para 'http://127.0.0.1:5000'.
+  API = ('/backend')
+
 } else {
-   API = ('https://sboard-api.onrender.com/')
-  
+  API = ('https://sboard-api.onrender.com/')
 }
 
 export const api = axios.create({
@@ -17,4 +18,3 @@ api.get('/')
 .then(response => console.info(response.data))
 .catch((err) => console.warn('Não foi possivel se conectar com a API, \n\n',err)
 )
-
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Redireciona requisições de /backend para o seu servidor Flask
+      '/backend': {
+        target: 'http://127.0.0.1:5000', // Endereço do seu backend
+        changeOrigin: true,
+        // Reescreve o caminho para remover /backend antes de enviar para o Flask
+        rewrite: (path) => path.replace(/^\/backend/, '') 
+      }
+    }
+  }
 })
